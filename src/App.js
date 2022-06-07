2e989b17a5e443eb3cc75bac50cbe6ace8681c2f
LvL090 <calvod879@gmail.com>
2022-06-07 11:46:58 +0200

    update section

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Clearbutton from './components/clearbutton';
 import Screen from './components/screen';
 import Title from './components/title';
 import DeleteButton from './components/delete';
-//import Section from './components/section';
+import Section from './components/Section';
 import { useState } from 'react';
 import { evaluate } from 'mathjs';
 
@@ -35,40 +35,40 @@ return (
         <div className='App h-screen'>
 
                 <div className='mx-auto overflow-hidden mt-2 shadow-lg mb-2 bg-slate-700 shadow-lg border rounded-lg lg:w-2/6 md:w-3/6 sm:w-4/6'>
-                <section>
+                <>
                         <Title />
                         <Screen input={input} />
-                </section>
-                <section className='flex items-stretch bg-slate-700 h-20'>      
+                </>
+                <Section>  
                         <Button handleClick={addInput}>%</Button>
                         <Button handleClick={addInput}>(</Button>
                         <Button handleClick={addInput}>)</Button>
                         <DeleteButton handleClick ={() => setInput ('')}>c</DeleteButton>
-                </section>
-                <section className='flex items-stretch bg-slate-700 h-20'>      
+                </Section>   
+                <Section>    
                         <Button handleClick={addInput}>7</Button>
                         <Button handleClick={addInput}>8</Button>
                         <Button handleClick={addInput}>9</Button>
                         <Button handleClick={addInput}>/</Button>
-                </section>
-                <section className='flex items-stretch bg-slate-700 h-20'>
+                </Section>
+                <Section>
                         <Button handleClick={addInput}>4</Button>
                         <Button handleClick={addInput}>5</Button>
                         <Button handleClick={addInput}>6</Button>
                         <Button handleClick={addInput}>*</Button>
-                </section>
-                <section className='flex items-stretch bg-slate-700 h-20'>
+                </Section>
+                <Section>
                         <Button handleClick={addInput}>1</Button>
                         <Button handleClick={addInput}>2</Button>
                         <Button handleClick={addInput}>3</Button>
                         <Button handleClick={addInput}>-</Button>
-                </section>
-                <section className='flex items-stretch bg-slate-700 h-20 mb-10'>
+                </Section>
+                <Section >
                         <Button handleClick={addInput}>0</Button>
                         <Button handleClick={addInput}>.</Button>
                         <EqualButton handleClick={calculateResult}>=</EqualButton>
                         <Button handleClick={addInput}>+</Button>
-                </section>
+                </Section>
                 <section className='flex items-stretch bg-slate-700 h-20'>
                         <Clearbutton handleClick ={() => setInput ('')}>Reset</Clearbutton>
                 </section>
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 
-function Section () {
+function Section ({children}) {
 return (
         
         <section className='flex items-stretch bg-slate-700 h-20'>
-
+        {children}
         </section>
         );
 }
